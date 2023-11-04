277707852d7d8f2e517c4a06fb6613dfd5ce4b87
Tkachuk-Yaroslav <tkachuky105@gmail.com>
2023-11-04 15:23:58 +0200

    start project

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
+import { Navigate, Route, Routes } from 'react-router-dom';
+import LayoutHome from './LayoutHome';
+import HomePage from 'pages/HomePage/HomePage';
+import RatesPage from 'pages/RatesPage/RatesPage';
+import { useEffect } from 'react';
+import { getCurrentPosition } from 'service/getCurrentPosition';
+
 export const App = () => {
+  useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+
+    function success(pos) {
+      getCurrentPosition(pos.coords);
+    }
+
+    function error(err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<LayoutHome />}>
+        <Route index element={<HomePage />} />
+        <Route path="rates" element={<RatesPage />} />
+      </Route>
+      <Route path="*" element={<Navigate to={'/'} />} />
+    </Routes>
   );
 };
---
+++ src/service/getCurrentPosition.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+export const getCurrentPosition = async ({ latitude, longitude }) => {
+  const apiKey = 'd4683b09d0c94ec0aebf0b2e043decbf';
+  const urlPosition = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}&language=en`;
+
+  const response = await axios.get(urlPosition);
+  console.log(response);
+};
